cb3324b69a4968c4f43e3e14696aad5e2eded428
veronikalotko <veronika.lotko@1nce.com>
2025-02-15 15:47:28 +0200

    fixed gradient bubbles

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,9 @@ function App() {
   return (
     <>
       <div className="background">
+        <GradientBubbles />
         <Header />
         <Home />
-        <GradientBubbles />
       </div>
     </>
   );
